9bb811b9beabd7a6bb603e3dc91dbf1f2e7e27d2
ClassyDragon <cgandeg@gmail.com>
2020-01-28 11:36:57 -0500

    Added test file

---
+++ test.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("canvas_element");
+const context = canvas.getContext('2d');
+
+context.fillStyle = '#229';
+context.fillRect(0, 0, canvas.width, canvas.height);
