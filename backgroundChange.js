925d5e03190032398d2ee567c08e28755998e48a
ClassyDragon <cgandeg@gmail.com>
2020-02-03 20:47:52 -0500

    Background slideshow, sidebar

---
+++ backgroundChange.js
@@ -0,0 +1,12 @@
+const images = ["url(\"images/library.jpg\")", "url(\"images/train_station.jpg\")", "url(\"images/bank.jpg\")", "url(\"images/code2.png\")"];
+let currentBackground = images.findIndex((str) => { return str == document.getElementById('Body').style.backgroundImage; }); 
+
+function advanceImage() {
+    console.log(document.getElementById('Body').style.backgroundImage);
+    console.log(currentBackground);
+    currentBackground++;
+    if (currentBackground >= images.length) {
+        currentBackground = 0;
+    }
+    document.getElementById('Body').style.backgroundImage = images[currentBackground];
+}
