68df7a44a3cf1698e47b68e45b8fd3ee6a99099c
ClassyDragon <cgandeg@gmail.com>
2020-04-06 14:25:15 -0400

    AC_NH

--- AC_NH/Bugs/script.js
+++ AC_NH/Bugs/script.js
@@ -1,8 +1,14 @@
 function populateList() {
     const list = document.getElementById("bug_list");
-    const jsonFile = $.getJSON("bugs.json", (json) => {
+    const bugs = $.getJSON("bugs.json", (json) => {
         console.log(json);
-    })
+    });
+    for (const bug of bugs) {
+        let node = document.createElement("LI");
+        let textNode = document.createTextNode(bug.id);
+        node.appendChild(textNode);
+        list.appendChild(node);
+    }
 }
 
 populateList();
