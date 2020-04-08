fa6cd2603ee6e7b0e186f068b5b51a3d7e7e94ac
ClassyDragon <cgandeg@gmail.com>
2020-04-08 15:19:26 -0400

    Bugs

--- AC_NH/Bugs/script.js
+++ AC_NH/Bugs/script.js
@@ -1,27 +1,43 @@
 function updateInfo(insect) {
+    document.getElementById("info_img").src = "img/" + insect.img_file;
     document.getElementById("insect_id").innerHTML = insect.id;
-    document.getElementById("insect_price").innerHTML = insect.price;
+    document.getElementById("insect_price").innerHTML = (Number(insect.price)).toLocaleString() + " bells";
+    document.getElementById("inventory_price").innerHTML = (Number(insect.price) * 40).toLocaleString() + " bells";
 }
 
-function populateList() {
+async function addBug(bug) {
     const list = document.getElementById("bug_list");
-    const bugs = $.getJSON("bugs.json", (json) => {
-        for (const bug of Object.values(json)) {
-            let node = document.createElement("LI");
-            let imgNode = document.createElement("IMG");
-            imgNode.src = "img/" + bug.img_file;
-            let textNode = document.createElement("P");
-            textNode.innerHTML = bug["id"];
-            node.onclick = () => {
-                updateInfo(bug);
-            }
-            if (bug.img_file != "?") {
-                node.appendChild(imgNode);
-            }
-            node.appendChild(textNode);
-            list.appendChild(node);
-        }
-    });
+
+    // Create the div and style:
+    let node = document.createElement("div");
+    node.setAttribute('class', 'bug');
+
+    // Create the paragraph:
+    let textNode = document.createElement("p");
+    textNode.innerHTML = bug["id"];
+
+    node.onclick = () => {
+        updateInfo(bug);
+    }
+
+    node.appendChild(textNode);
+    list.appendChild(node);
 }
 
-populateList();
+
+function initialize() {
+    const initbug = {
+        "id": "Common butterfly",
+        "img_file": "common_butterfly.png",
+        "price": "160"
+    };
+    updateInfo(initbug);
+}
+
+$.getJSON("https://gaiablade.github.io/AC_NH/Bugs/bugs.json", (json) => {
+    for (const bug of Object.values(json)) {
+        addBug(bug);
+    }
+})
+
+initialize();
