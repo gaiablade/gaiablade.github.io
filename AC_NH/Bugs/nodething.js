9d81c840b2e8cb340b6d71485504e6b4e736e38e
ClassyDragon <cgandeg@gmail.com>
2020-04-08 16:29:34 -0400

    Bug Info

--- AC_NH/Bugs/nodething.js
+++ AC_NH/Bugs/nodething.js
@@ -1,208 +1,489 @@
 let json = {
-    "common_butterfly": {
-        "id": "Common butterfly",
-        "img_file": "common_buttefly.png",
-        "price": "?"
-    },
-    "yellow_butterfly": {
-        "id": "Yellow butterfly",
-        "img_file": "yellow_butterfly.png",
-        "price": "?"
-    },
-    "tiger_butterfly": {
-        "id": "Tiger butterfly",
-        "img_file": "tiger_butterfly.png",
-        "price": "?"
-    },
-    "peacock_butterfly": {
-        "id": "Peacock butterfly",
-        "img_file": "peacock_butterfly.png",
-        "price": "2500"
-    },
-    "common_bluebottle": {
-        "id": "Common bluebottle",
-        "img_file": "common_bluebottle.png",
-        "price": "?"
-    },
-    "paper_kite_butterfly": {
-        "id": "Paper kite butterfly",
-        "img_file": "paper_kite_butterfly.png",
-        "price": "?"
-    },
-    "?1": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?2": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "emperor_butterfly": {
-        "id": "Emperor butterfly",
-        "img_file": "emperor_butterfly.png",
-        "price": "4000"
-    },
-    "agrias_butterfly": {
-        "id": "Agrias butterfly",
-        "img_file": "agrias_butterfly.png",
-        "price": "?"
-    },
-    "rajah_brooke's_birdwing": {
-        "id": "Rajah Brooke's birdwing",
-        "img_file": "rajah_brookes_birdwing.png",
-        "price": "?"
-    },
-    "?3": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "moth": {
-        "id": "Moth",
-        "img_file": "moth.png",
-        "price": "?"
-    },
-    "atlas_moth": {
-        "id": "Atlas moth",
-        "img_file": "atlas_moth.png",
-        "price": "?"
-    },
-    "madagascan_sunset_moth": {
-        "id": "Madagascan sunset moth",
-        "img_file": "madagascan_sunset_moth.png",
-        "price": "?"
-    },
-    "long_locust": {
-        "id": "Long locust",
-        "img_file": "long_locust.png",
-        "price": "?"
-    },
-    "?4": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?5": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?6": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?7": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?8": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "mantis": {
-        "id": "Mantis",
-        "img_file": "mantis.png",
-        "price": "?"
-    },
-    "orchid_mantis": {
-        "id": "Orchid mantis",
-        "img_file": "orchid_mantis.png",
-        "price": "?"
-    },
-    "honeybee": {
-        "id": "Honeybee",
-        "img_file": "honeybee.png",
-        "price": "?"
-    },
-    "wasp": {
-        "id": "Wasp",
-        "img_file": "wasp.png",
-        "price": "?"
-    },
-    "?9": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?10": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?11": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?12": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?13": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?14": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?15": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "darner_dragonfly": {
-        "id": "Darner dragonfly",
-        "img_file": "darner_dragonfly.png",
-        "price": "?"
-    },
-    "?16": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?17": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?18": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?19": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?20": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "?21": {
-        "id": "?",
-        "img_file": "?",
-        "price": "?"
-    },
-    "giant_water_bug": {
-        "id": "Giant water bug",
-        "img_file": "giant_water_bug.png",
-        "price": "?"
-    }
+  "common_butterfly": {
+    "id": "Common butterfly",
+    "img_file": "common_butterfly.png",
+    "price": "160",
+    "location": ""
+  },
+  "yellow_butterfly": {
+    "id": "Yellow butterfly",
+    "img_file": "yellow_butterfly.png",
+    "price": "160",
+    "location": ""
+  },
+  "tiger_butterfly": {
+    "id": "Tiger butterfly",
+    "img_file": "tiger_butterfly.png",
+    "price": "240",
+    "location": ""
+  },
+  "peacock_butterfly": {
+    "id": "Peacock butterfly",
+    "img_file": "peacock_butterfly.png",
+    "price": "2500",
+    "location": ""
+  },
+  "common_bluebottle": {
+    "id": "Common bluebottle",
+    "img_file": "common_bluebottle.png",
+    "price": "300",
+    "location": ""
+  },
+  "paper_kite_butterfly": {
+    "id": "Paper kite butterfly",
+    "img_file": "paper_kite_butterfly.png",
+    "price": "1000",
+    "location": ""
+  },
+  "?1": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?2": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "emperor_butterfly": {
+    "id": "Emperor butterfly",
+    "img_file": "emperor_butterfly.png",
+    "price": "4000",
+    "location": ""
+  },
+  "agrias_butterfly": {
+    "id": "Agrias butterfly",
+    "img_file": "agrias_butterfly.png",
+    "price": "3000",
+    "location": ""
+  },
+  "rajah_brooke's_birdwing": {
+    "id": "Rajah Brooke's birdwing",
+    "img_file": "rajah_brookes_birdwing.png",
+    "price": "2500",
+    "location": ""
+  },
+  "?3": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "moth": {
+    "id": "Moth",
+    "img_file": "moth.png",
+    "price": "130",
+    "location": ""
+  },
+  "atlas_moth": {
+    "id": "Atlas moth",
+    "img_file": "atlas_moth.png",
+    "price": "3000",
+    "location": ""
+  },
+  "madagascan_sunset_moth": {
+    "id": "Madagascan sunset moth",
+    "img_file": "madagascan_sunset_moth.png",
+    "price": "2500",
+    "location": ""
+  },
+  "long_locust": {
+    "id": "Long locust",
+    "img_file": "long_locust.png",
+    "price": "200",
+    "location": ""
+  },
+  "?4": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?5": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?6": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?7": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?8": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "mantis": {
+    "id": "Mantis",
+    "img_file": "mantis.png",
+    "price": "430",
+    "location": ""
+  },
+  "orchid_mantis": {
+    "id": "Orchid mantis",
+    "img_file": "orchid_mantis.png",
+    "price": "2400",
+    "location": ""
+  },
+  "honeybee": {
+    "id": "Honeybee",
+    "img_file": "honeybee.png",
+    "price": "200",
+    "location": ""
+  },
+  "wasp": {
+    "id": "Wasp",
+    "img_file": "wasp.png",
+    "price": "2500",
+    "location": ""
+  },
+  "?9": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?10": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?11": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?12": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?13": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?14": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?15": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "darner_dragonfly": {
+    "id": "Darner dragonfly",
+    "img_file": "darner_dragonfly.png",
+    "price": "230",
+    "location": ""
+  },
+  "?16": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?17": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?18": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?19": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?20": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?21": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "giant_water_bug": {
+    "id": "Giant water bug",
+    "img_file": "giant_water_bug.png",
+    "price": "2000",
+    "location": ""
+  },
+  "stinkbug": {
+    "id": "Stinkbug",
+    "img_file": "stinkbug.png",
+    "price": "120",
+    "location": ""
+  },
+  "man-faced_stink_bug": {
+    "id": "Man-faced stink bug",
+    "img_file": "man-faced_stink_bug.png",
+    "price": "1000",
+    "location": ""
+  },
+  "ladybug": {
+    "id": "Ladybug",
+    "img_file": "ladybug.png",
+    "price": "200",
+    "location": ""
+  },
+  "tiger_beetle": {
+    "id": "Tiger beetle",
+    "img_file": "tiger_beetle.png",
+    "price": "1500",
+    "location": ""
+  },
+  "jewel_beetle": {
+    "id": "Jewel beetle",
+    "img_file": "jewel_beetle.png",
+    "price": "2400",
+    "location": ""
+  },
+  "?220": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "citrus_long-horned_beetle": {
+    "id": "Citrus long-horned beetle",
+    "img_file": "citrus_long-horned_beetle.png",
+    "price": "350",
+    "location": ""
+  },
+  "?22": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?23": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?24": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?25": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?26": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?27": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?28": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?29": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?30": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?31": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?32": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?33": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?34": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?35": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?36": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?37": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?38": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?39": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?40": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?41": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "bagworm": {
+    "id": "Bagworm",
+    "img_file": "bagworm.png",
+    "price": "600",
+    "location": ""
+  },
+  "?42": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "hermit_crab": {
+    "id": "Hermit crab",
+    "img_file": "hermit_crab.png",
+    "price": "1000",
+    "location": ""
+  },
+  "wharf_roach": {
+    "id": "Wharf roach",
+    "img_file": "wharf_roach.png",
+    "price": "200",
+    "location": ""
+  },
+  "?43": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "?44": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  },
+  "flea": {
+    "id": "Flea",
+    "img_file": "flea.png",
+    "price": "70",
+    "location": ""
+  },
+  "snail": {
+    "id": "Snail",
+    "img_file": "snail.png",
+    "price": "250",
+    "location": ""
+  },
+  "pill_bug": {
+    "id": "Pill bug",
+    "img_file": "pill_bug.png",
+    "price": "250",
+    "location": ""
+  },
+  "centipede": {
+    "id": "Centipede",
+    "img_file": "centipede.png",
+    "price": "300",
+    "location": ""
+  },
+  "spider": {
+    "id": "Spider",
+    "img_file": "spider.png",
+    "price": "480",
+    "location": ""
+  },
+  "tarantula": {
+    "id": "Tarantula",
+    "img_file": "tarantula.png",
+    "price": "8000",
+    "location": ""
+  },
+  "?45": {
+    "id": "?",
+    "img_file": "?",
+    "price": "?",
+    "location": ""
+  }
 };
 
 for (let obj of Object.values(json)) {
-    obj["location"] = "";
+    obj["months"] = "";
+    obj["time"] = "";
 }
 
 let string = JSON.stringify(json, null, 2);
